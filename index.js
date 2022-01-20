fa123a9663d752d26b4208d6e13ffc5b67c7f781
Gustavo Conde <guoconde@pop-os.localdomain>
2022-01-20 10:33:15 -0300

    Finalizando Exercicio Extra_01

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from 'express'
+
+const server = express()
+
+const holidays = [
+    { date: "1/1/2022", name: "Confraternização mundial" },
+    { date: "1/3/2022", name: "Carnaval" },
+    { date: "4/17/2022", name: "Páscoa" },
+    { date: "4/21/2022", name: "Tiradentes" },
+    { date: "5/1/2022", name: "Dia do trabalho" },
+    { date: "6/16/2022", name: "Corpus Christi" },
+    { date: "9/7/2022", name: "Independência do Brasil" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+    { date: "11/2/2022", name: "Finados" },
+    { date: "11/15/2022", name: "Proclamação da República" },
+    { date: "12/25/2022", name: "Natal" }
+];
+
+const data = new Date();
+const hoje = data.toLocaleDateString('en-US')
+
+server.get('/holidays', (req, res) => {
+    res.send(holidays)
+})
+
+server.get('/is-today-holiday', (req, res) => {
+
+    let msg = ''
+
+    for (let i = 0; i < holidays.length; i++) {
+        if (holidays[i].date == hoje) {
+            msg = `Sim, hoje é nome-do-feriado ${holidays[i].name}`
+            break
+        } else {
+            msg = 'Não, hoje não é feriado'
+        }
+    }
+
+    res.send(msg)
+})
+
+server.get('/holidays/:idMes', (req, res) => {
+    
+    const mes = req.params.idMes
+
+    const regex =  /([0-9]{1,2})/
+    
+    const feriados = []
+
+    for (let i = 0; i < holidays.length; i++) {
+        let dia = holidays[i].date
+
+        if(regex.exec(dia)[0] == mes) {
+            feriados.push(holidays[i].name)
+
+        }
+    }
+
+    res.send(feriados)
+})
+
+server.listen(4000)
